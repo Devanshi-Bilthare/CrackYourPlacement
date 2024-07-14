19c75eaf6149f8ee0774a701c6b94877cc87970a
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2024-07-14 21:01:47 +0530

    sort colors

---
+++ Day1/sort-colors.java
@@ -0,0 +1,49 @@
+// Bubble sort
+// tc - O(N^2)
+class Solution {
+    public void sortColors(int[] nums) {
+       int n =nums.length;
+
+       for(int i =0;i < n;i++){
+        int min = i;
+
+        for(int j = i+1;j < n;j++){
+            if(nums[j] < nums[min]) min = j;
+        }
+
+            int temp = nums[i];
+            nums[i] = nums[min];
+            nums[min] = temp;
+       }
+        
+    }
+}
+
+// optimized approach
+// tc - O(N)
+
+class Solution {
+    public void sortColors(int[] nums) {
+     int n = nums.length;
+     int zero = 0;
+     int one = 0;
+     int two = n-1;
+
+     while(one <= two){
+        if(nums[one] == 0){
+            int temp = nums[one];
+            nums[one] = nums[zero];
+            nums[zero] = temp;
+            zero++;
+            one++;
+        }else if(nums[one] == 1){
+            one++;
+        }else{
+            int temp = nums[one];
+            nums[one] = nums[two];
+            nums[two] = temp;
+            two--;
+        }
+     }
+    }
+}
